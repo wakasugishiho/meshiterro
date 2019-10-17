df45c8ab0767982569b62dbdafc206b69886bbe4
若杉志帆 <wakasugishiho@wakasugishihonoMacBook-Air.local>
2019-10-17 14:31:01 +0900

    [finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8cbd84a336c19d92aab906ed2fb8875aa899af503f123cb68375bfd93c63efa6fb28d3bbb5d86dd27b3c603fc4d6678d5c2198107a83236e4c778b0864536ada'
